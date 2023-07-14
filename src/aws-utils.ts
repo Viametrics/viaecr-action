ead190b68fa27edd8216627fae0d62a606020de0
sjobergjim <sjoberg.jim@gmail.com>
2023-07-14 15:25:11 +0200

    Refactor configs & remove unused deps
    Remove jest (testing is nice but unused atm)
    
    bump node version in CI scripts & match with types
    
    prettier-ignore .github/
    
    remove residual jest config
    
    Make prettier format whole project & format project
    
    prettier-ignore IDE settings
    
    Change eslint & prettier configs
    
    format project
    
    reconfig prettier & format

--- src/aws-utils.ts
+++ src/aws-utils.ts
@@ -1,53 +1,47 @@
 import {
-  DescribeImagesCommand,
-  DescribeRepositoriesCommand,
-  DescribeRepositoriesCommandOutput,
-  ECRClient,
-  ImageNotFoundException,
-} from '@aws-sdk/client-ecr'
-import {actionInput} from './action-input'
+	DescribeImagesCommand,
+	DescribeRepositoriesCommand,
+	DescribeRepositoriesCommandOutput,
+	ECRClient,
+	ImageNotFoundException,
+} from "@aws-sdk/client-ecr";
+import { actionInput } from "./action-input";
 
 export const awsCredentials = {
-  accessKeyId: actionInput.awsAccessKeyId,
-  secretAccessKey: actionInput.awsSecretAccessKey,
-}
+	accessKeyId: actionInput.awsAccessKeyId,
+	secretAccessKey: actionInput.awsSecretAccessKey,
+};
 
 export const ecrConfig = {
-  credentials: awsCredentials,
-  region: actionInput.awsRegion,
-}
+	credentials: awsCredentials,
+	region: actionInput.awsRegion,
+};
 
-export const ecrClient = new ECRClient(ecrConfig)
+export const ecrClient = new ECRClient(ecrConfig);
 
 export async function getEcrRegistry(): Promise<string> {
-  const response: DescribeRepositoriesCommandOutput = await ecrClient.send(
-    new DescribeRepositoriesCommand({}),
-  )
+	const response: DescribeRepositoriesCommandOutput = await ecrClient.send(new DescribeRepositoriesCommand({}));
 
-  const repositoryUri: string | undefined =
-    response?.repositories?.[0]?.repositoryUri?.split('/')[0]
+	const repositoryUri: string | undefined = response?.repositories?.[0]?.repositoryUri?.split("/")[0];
 
-  if (repositoryUri) {
-    return repositoryUri
-  } else {
-    throw new Error('No repositories found')
-  }
+	if (repositoryUri) {
+		return repositoryUri;
+	} else {
+		throw new Error("No repositories found");
+	}
 }
 
-export async function checkIfEcrImageExists(
-  repository: string,
-  imageTag: string,
-): Promise<void> {
-  try {
-    const request = new DescribeImagesCommand({
-      repositoryName: repository,
-      imageIds: [{imageTag}],
-    })
+export async function checkIfEcrImageExists(repository: string, imageTag: string): Promise<void> {
+	try {
+		const request = new DescribeImagesCommand({
+			repositoryName: repository,
+			imageIds: [{ imageTag }],
+		});
 
-    await ecrClient.send(request)
-  } catch (e) {
-    if (e instanceof ImageNotFoundException) return
-  }
+		await ecrClient.send(request);
+	} catch (e) {
+		if (e instanceof ImageNotFoundException) return;
+	}
 
-  throw new Error(`Image with tag ${imageTag} already exists`)
+	throw new Error(`Image with tag ${imageTag} already exists`);
 }
