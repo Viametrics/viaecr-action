8561d25296f486227aefea9046e9e547fa3f76c1
Alexander Rundberg <alexanderrundberg@fastmail.jp>
2023-08-30 14:50:47 +0200

    add option to disable buildkit (default is enabled)

--- src/main.ts
+++ src/main.ts
@@ -6,32 +6,38 @@ import { Docker } from "docker-cli-js";
 import { actionInput } from "./action-input";
 
 async function run(): Promise<void> {
-	const registry: string = await getEcrRegistry();
-	const dockerfile: string = actionInput.dockerfile;
+  const registry: string = await getEcrRegistry();
+  const dockerfile: string = actionInput.dockerfile;
 
-	const repository: string = actionInput.repository;
-	const imageTag: string = createImageTag();
-	await checkIfEcrImageExists(repository, imageTag);
+  const repository: string = actionInput.repository;
+  const imageTag: string = createImageTag();
+  await checkIfEcrImageExists(repository, imageTag);
 
-	// Build and push image
-	const regRepoImg = `${registry}/${repository}:${imageTag}`;
-	const dockerBuild = `build ${actionInput.buildArgs} -t ${regRepoImg} -f ./${dockerfile} .`;
+  // Build and push image
+  const regRepoImg = `${registry}/${repository}:${imageTag}`;
+  const dockerBuild = `build ${actionInput.buildArgs} -t ${regRepoImg} -f ./${dockerfile} .`;
 
-	const docker = new Docker();
-	await docker.command(dockerBuild);
-	await docker.command(`push ${regRepoImg}`);
+  // Enable or disable buildkit
+  core.exportVariable(
+    "DOCKER_BUILDKIT",
+    actionInput.disableBuildkit ? "0" : "1",
+  );
+
+  const docker = new Docker();
+  await docker.command(dockerBuild);
+  await docker.command(`push ${regRepoImg}`);
 }
 
 function createImageTag(): string {
-	const shortSha: string = context.sha.substring(0, 7);
-	const timestamp: string = format(new Date(), "yyyyMMdd-HHmm");
-	return `${actionInput.tagPrefix}${timestamp}-${shortSha}`;
+  const shortSha: string = context.sha.substring(0, 7);
+  const timestamp: string = format(new Date(), "yyyyMMdd-HHmm");
+  return `${actionInput.tagPrefix}${timestamp}-${shortSha}`;
 }
 
 try {
-	run();
+  run();
 } catch (error) {
-	if (error instanceof Error) {
-		core.setFailed(error.message);
-	}
+  if (error instanceof Error) {
+    core.setFailed(error.message);
+  }
 }
